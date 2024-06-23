9c4dcbf6e8b38045e62c88e4dadb5f23c897d05c
SantoshRanbhare <santoshranbhare60@gmail.com>
2024-06-23 17:54:59 +0530

    Inital commit

--- src/main/java/com/example/cms/ContentManagementSyatemApplication.java
+++ src/main/java/com/example/cms/ContentManagementSyatemApplication.java
@@ -2,9 +2,13 @@ package com.example.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import jakarta.servlet.annotation.WebServlet;
 
 
-
+@EnableScheduling
+@WebServlet(name = "index.html")
 @SpringBootApplication
 public class ContentManagementSyatemApplication {
 	
